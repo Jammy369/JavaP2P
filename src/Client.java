a83b3d3067c5e3423db4e7fce1cc96621b2a9490
Jammy <labeyrieflorian@gmail.com>
2020-01-08 17:34:13 +0100

    Update TD 08012020

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,31 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			Socket socketEmission = new Socket("localhost", 2222);
+			
+			
+		} catch (UnknownHostException e) {
+			
+			
+		} catch (IOException e) {
+			
+			
+		}
+		
+		
+		
+		
+	}
 	
 }
